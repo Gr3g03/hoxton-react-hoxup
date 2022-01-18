744000b6cefaff65b695ba4eda230e60d842bce5
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-01-18 18:05:21 +0100

    working with get user + messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import MainPage from "./pages/MainPage";
-
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from "./pages/LoginPage";
 
 export default function App() {
-  return < MainPage />
 
+  return (
+    <Routes>
+      <Route index element={<Navigate replace to="/login" />} />
+      <Route path="login" element={<Login />} />
+      <Route path="logedin" element={<MainPage />} />
+
+    </Routes>
+
+
+  )
 }
