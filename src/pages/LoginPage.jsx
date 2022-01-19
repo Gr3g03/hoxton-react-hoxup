bef4b760f6721ced64aa1c5c96ba2f5ed3ce0fca
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-01-19 12:48:22 +0100

    working with logedin login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, Navigate, useNavigate, } from "react-router-dom"
 
 
 export default function Login() {
 
     const [users, setUsers] = useState([])
+    const [logedin, setLogedin] = useState([])
+
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`http://localhost:4000/users`)
             .then(resp => resp.json())
             .then(getUsersFromServer => setUsers(getUsersFromServer))
     }, [])
+
+
+    function login(id, users) {
+        return fetch(`http://localhost:4000/users/${id}`)
+            .then(resp => resp.json())
+            .then((user) => {
+                if (user.id === users) {
+
+                    // let logedinUser = JSON.parse(JSON.stringify(users))
+                    // const match = logedinUser.find((target) => target.id === users.id)
+                    // setLogedin(logedinUser)
+
+
+
+
+                }
+            })
+    }
+
+    console.log(logedin)
 
     function addANewUser(firstName, lastName, phoneNumber, avatar) {
         return fetch("http://localhost:4000/users", {
@@ -40,25 +64,31 @@ export default function Login() {
                     {
                         users.map(user =>
                             <li key={user.id}>
-                                <Link to={"/logedin"}>
-                                    <button className="user-selection"
-                                    >
-                                        <img
-                                            className="avatar"
-                                            width="50"
-                                            height="50"
-                                            src={user.avatar}
-                                            alt={user.firstName}
-                                        />
-                                        <h3>{user.firstName}  {user.lastName}</h3>
-                                    </button>
-                                </Link>
+                                {/* <Link to={"/logedin"}> */}
+                                <button className="user-selection"
+                                    onClick={function () {
+
+                                        let logedinUser = [...logedin, users]
+                                        navigate('/logedin')
+                                        setLogedin(logedinUser)
+                                    }}
+                                >
+                                    <img
+                                        className="avatar"
+                                        width="50"
+                                        height="50"
+                                        src={user.avatar}
+                                        alt={user.firstName}
+                                    />
+                                    <h3>{user.firstName}  {user.lastName}</h3>
+                                </button>
+                                {/* </Link> */}
                             </li>
 
                         )}
 
                     <li>
-                        <button className="user-selection"><h3>+ Add a new user</h3></button>
+                        <button className="user-selection" onChange={() => addANewUser}><h3>+ Add a new user</h3></button>
                     </li>
 
                 </ul>
